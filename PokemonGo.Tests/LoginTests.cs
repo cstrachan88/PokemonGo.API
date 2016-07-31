fc3cdf17b6c25c938b4b15af38706baaec24fbaf
Courtney Strachan <courtney.strachan@gmail.com>
2016-07-30 18:22:28 -0700

    Added test methods for logging in

--- PokemonGo.Tests/LoginTests.cs
+++ PokemonGo.Tests/LoginTests.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokemonGo.API;
+using PokemonGo.API.Enums;
+using PokemonGo.API.Exceptions;
 
 namespace PokemonGo.Tests
 {
@@ -7,9 +12,55 @@ namespace PokemonGo.Tests
     public class LoginTests
     {
         [TestMethod]
-        public void LoginTest1()
+        public async Task BadLoginTest()
         {
+            var client = new Client(new Settings
+            {
+                AuthType = AuthType.Google,
+                Latitude = 0,
+                Longitude = 0,
+                Altitude = 0,
+                Username = "fake@gmail.com",
+                Password = "fake"
+            });
 
+            try
+            {
+                await client.Login.DoLogin();
+
+                Assert.Fail("Expected a GoogleException");
+            }
+            catch (GoogleException)
+            {
+                Debug.WriteLine("Failed with a GoogleException (expected)");
+            }
+        }
+
+        [TestMethod]
+        public async Task GoodLoginTest()
+        {
+            // Fill in your login information here...
+
+            var client = new Client(new Settings
+            {
+                AuthType = AuthType.Google,
+                Latitude = 0,
+                Longitude = 0,
+                Altitude = 0,
+                Username = "real@gmail.com",
+                Password = "real"
+            });
+
+            try
+            {
+                await client.Login.DoLogin();
+                Debug.WriteLine("Logged in with auth token: " + client.AuthToken);
+            }
+            catch (GoogleException)
+            {
+                Debug.WriteLine("Check to make sure your login information is correct.");
+                throw;
+            }
         }
     }
 }
