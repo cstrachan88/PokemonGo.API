e771a701f4c7c7e819f1bae422d0ed57f13e6884
Courtney Strachan <courtney.strachan@gmail.com>
2016-07-30 17:55:10 -0700

    Added test project

---
+++ PokemonGo.Tests/LoginTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PokemonGo.Tests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestMethod]
+        public void LoginTest1()
+        {
+
+        }
+    }
+}
